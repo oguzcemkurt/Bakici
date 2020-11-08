959886aa2d2a238b2238e923df800bac6d554908
Oğuz Cem Kurt <ouzcem@gmail.com>
2020-11-08 15:49:21 +0300

    OOP

---
+++ BakiciMod.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import os
+
+class BakiciMod:
+    def __init__(self):
+        self.konum = os.getcwd()
+        self.konumunuz = "Konumunuz: " + self.konum
+
+    def dosyalarSet(self):
+        gelenAdres = raw_input("Kontrol edilecek dizin yolu giriniz: ")
+
+        gelenUzanti = raw_input("Aranacak dosya uzantısı: ")
+        dosyalar = os.listdir(gelenAdres)
+
+        self.uzantilarGrup=[]
+        for dosya in dosyalar:
+            if dosya.endswith(gelenUzanti):
+                self.uzantilarGrup.append(dosya)
+
+    def dosyalarGet(self):
+        self.dosyalarSet()
+
+        for bulunanDosyalar in self.uzantilarGrup:
+            print bulunanDosyalar
+
--- bakici.py
+++ bakici.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 # coding=utf-8
-import os
-konum = os.getcwd();
+import BakiciMod
 
-print "Konumunuz: " + konum
-gelenAdres = raw_input("Kontrol edilecek dizin yolu giriniz: ")
+Bakici = BakiciMod.BakiciMod()
 
-gelenUzanti = raw_input("Aranacak dosya uzantısı: ")
-dosyalar = os.listdir(gelenAdres)
+print Bakici.konumunuz
 
-for dosya in dosyalar:
-    if dosya.endswith(gelenUzanti):
-        print dosya
+Bakici.dosyalarGet()
+
 
 
